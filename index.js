f12694b283daf087928b23922a67822bc7a0852c
Michael Korostelev <korostelevm@gmail.com>
2021-10-15 12:50:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     console.log("A GET request!")
-    res.json({Hello:'asdfasdfasdfsdf'})
+    res.json({Hello:Math.random()})
 })
 app.listen(process.env.PORT || 3000)
           
