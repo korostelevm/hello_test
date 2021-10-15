db5020e83e74815d76fb8084f636753282e63b73
Michael Korostelev <korostelevm@gmail.com>
2021-10-15 12:51:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    console.log(req.headersss)
+    console.log(headersss)
     console.log("A GET request!")
     res.json({Hello:Math.random()})
 })
