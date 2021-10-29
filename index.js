5be47e00e5819c9a45a17932ecf4b4ca06d0ad3a
Michael Korostelev <korostelevm@gmail.com>
2021-10-29 13:16:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.json({
         Hello:"World",
         var: Math.random()*100,
-        asdfasf:'asdfasf'   })
+        asdfasf:'444444'   })
 })
 app.listen(process.env.PORT || 3000)
           
