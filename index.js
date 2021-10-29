76c6a56fcffebf9b74c117630c21583507668d3b
Michael Korostelev <korostelevm@gmail.com>
2021-10-29 13:48:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ app.get('/', (req, res) => {
     console.log("A GET Request !")
     
     res.json({
-        Hello:"World",
-        var: Math.random()*100,
-        asdfasf:'ffff'   })
+        Hello:"[THE WHOLE] World",
+        random: (Math.random()*100).toFixed(2),
+        asdfasdf:'https://app.cyclic.sh/'   })
 })
 app.listen(process.env.PORT || 3000)
           
